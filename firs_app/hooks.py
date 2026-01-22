8f8259f2cfb826db870dfaa3357afd79267ee581
lajidey <spryng.managed@gmail.com>
2026-01-22 13:01:59 +0000

    FIRS Invoice Schema Dafting
    Building firs sales invoice schema adding dafting all moving parts item vat list, supplier, customer tin and more..

--- firs_app/hooks.py
+++ firs_app/hooks.py
@@ -137,13 +137,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+    "Sales Invoice": {
+        "on_update": "firs_app.utils.firs_invoice.firs_work_flow_draft"
+    }
+}
 
 # Scheduled Tasks
 # ---------------
